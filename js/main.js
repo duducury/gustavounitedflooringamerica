31d3a55ebdee9a508c5eb52602906a1c7f011eb7
duducury <duducury_fjv@hotmail.com>
2026-05-03 17:30:19 -0400

    Fix estimate-cta mobile: navy shell to avoid white scroll flash

--- js/main.js
+++ js/main.js
@@ -264,7 +264,7 @@
       layer.setAttribute("aria-hidden", "true");
 
       layer.style.cssText =
-        "position:fixed;top:0;left:0;right:0;bottom:0;width:100%;height:100%;min-height:100dvh;pointer-events:none;overflow:hidden;z-index:37;background-color:transparent;visibility:hidden";
+        "position:fixed;top:0;left:0;right:0;bottom:0;width:100%;height:100%;min-height:100dvh;pointer-events:none;overflow:hidden;z-index:37;visibility:hidden";
 
       var overlayImg = document.createElement("img");
       overlayImg.alt = "";
